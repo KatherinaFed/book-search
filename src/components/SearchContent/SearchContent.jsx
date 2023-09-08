8c309132f9b75b775e23e5928c7099ac06727fa8
Katherina <fedoseeva2292@gmail.com>
2023-09-08 11:49:40 +0200

    added selectors: category, sorting by

--- src/components/SearchContent/SearchContent.jsx
+++ src/components/SearchContent/SearchContent.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import cls from './SearchContent.module.css'
 import Title from '../../shared/Title/Title';
 import SearchForm from '../../features/SearchForm/SearchForm';
+import Selectors from '../../features/Selectors/Selectors';
 
 const SearchContent = () => {
   return (
     <div className={cls.searchContent}>
      <Title />
-     <div>
-
      <SearchForm />
-     </div>
+     <Selectors />
     </div>
   );
 };
