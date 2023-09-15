d3e523b05cfc2c6bd4824bc8415b76a8272565a7
Katherina <fedoseeva2292@gmail.com>
2023-09-15 19:14:20 +0200

    test: SearchForm; added smallBookLoader.gif

--- src/components/SearchContent/SearchContent.jsx
+++ src/components/SearchContent/SearchContent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import cls from './SearchContent.module.css';
-import Title from '../../shared/Title/Title';
+import Title from '../../shared/ui/Title/Title';
 import SearchForm from '../../features/SearchForm/SearchForm';
 import Selectors from '../../features/Selectors/Selectors';
 
---
+++ src/features/SearchForm/SearchForm.test.js
@@ -0,0 +1,51 @@
+import {
+  screen,
+  render,
+  act,
+  fireEvent,
+  getByRole,
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import SearchForm from './SearchForm';
+import React, { useState } from 'react';
+
+describe('Search Form component', () => {
+  it('should have - input, button', () => {
+    render(<SearchForm />);
+
+    const inputField = screen.getByPlaceholderText(/Search a book.../i);
+    const buttonSubmit = screen.getByRole('button');
+
+    expect(inputField).toBeInTheDocument();
+    expect(buttonSubmit).toBeInTheDocument();
+  });
+
+  it('form should update state and clear input', async () => {
+    const testInput = 'Harry Potter';
+    const setSearchData = jest.fn();
+    const setSearchBook = jest.fn();
+    const setStartIndex = jest.fn();
+
+    jest
+      .spyOn(React, 'useState')
+      .mockImplementation((init) => [init, setSearchBook]);
+
+    render(
+      <SearchForm setSearchData={setSearchData} setStartIndex={setStartIndex} />
+    );
+
+    await act(async () => {
+      const searchInput = screen.getByPlaceholderText(/Search a book.../i);
+
+      fireEvent.change(searchInput, { target: { value: testInput } });
+    });
+
+    await act(async () => {
+      const searchButton = screen.getByRole('button');
+      fireEvent.click(searchButton);
+    });
+
+    expect(setSearchData).toHaveBeenCalledWith(testInput);
+    expect(setStartIndex).toHaveBeenCalledWith(0);
+  });
+});
